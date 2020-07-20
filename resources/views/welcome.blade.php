3012c53d415d97940bd9359bf9a1c9e596d4ff09
Toshihisa-Handa <waykiyoharu@yahoo.co.jp>
2020-07-20 09:54:26 +0900

    Laravel5.8を6へ変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Larave5.8
+                    Larave6
                 </div>
 
                 <div class="links">
