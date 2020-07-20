c0fc1ab542b4cf075b6e0904e11a7da7f2bf5496
babahito <babahito@gmail.com>
2020-07-20 09:54:35 +0900

    LaravelをBABAvelに

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Larave5.8
+                    BABA_laravel
                 </div>
 
                 <div class="links">
